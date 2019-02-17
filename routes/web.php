fc186c4233a063de356e94caea24a8702d9b76a4
yeyulmq <3423437316@qq.com>
2019-02-17 04:05:21 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 // 用户相关
 Route::resource('users', 'UsersController', ['only'=>['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);

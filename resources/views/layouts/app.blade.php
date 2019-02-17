22fafd092e2bb6c0d845de5a444a11d8d71f0dc7
yeyulmq <3423437316@qq.com>
2019-02-17 07:40:35 +0000

    WYSIWYG编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,14 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  
+
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
-  
+
   <!-- Styles -->
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  @yield('styles')
   </head>
   <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -24,4 +25,5 @@
 
     <!-- Script -->
     <script src="{{ mix('js/app.js') }}"></script>
+    @yield('scripts')
   </body>
